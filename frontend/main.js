a57ad96eaee514f0baa7186a56107f9fc6685712
LimitToInfinity <21stLimitless@gmail.com>
2020-04-07 23:59:44 -0600

    Creates view for interacting with chat app using web sockets.

---
+++ frontend/main.js
@@ -0,0 +1,73 @@
+const webSocketURL = "ws://our-first-websocket.herokuapp.com"
+
+const connectForm = document.querySelector(".connect");
+const sendForm = document.querySelector(".send");
+const chatBox = document.querySelector("section");
+const leave = document.querySelector(".leave");
+
+connectForm.addEventListener("submit", handleConnect);
+sendForm.addEventListener("submit", handleSend);
+leave.addEventListener("click", handleLeave);
+
+const createWebSocket = () => {
+    const newWebSocket = new WebSocket(webSocketURL);
+    newWebSocket.onmessage = handleMessage;
+    return new Promise(resolve => waitForReadyState(resolve, newWebSocket));
+}
+
+function waitForReadyState(resolve, newWebSocket) {
+    console.log("wait");
+    if (newWebSocket.readyState === 1) { return resolve(newWebSocket); }
+    else { setTimeout(() => waitForReadyState(resolve, newWebSocket), 40); }
+}
+
+let webSocket;
+createWebSocket().then(socket => webSocket = socket);
+
+function handleMessage(message) {
+    const newChat = document.createElement("p");
+    newChat.textContent = message.data;
+    chatBox.prepend(newChat);
+}
+
+let name;
+function handleConnect(event) {
+    event.preventDefault();
+
+    if (webSocket.readyState === 1) {
+        enterUserIntoChat(event);
+    } else {
+        createWebSocket().then(socket => {
+            webSocket = socket;
+            handleConnect(event);
+        });
+    }
+}
+
+function enterUserIntoChat(event) {
+    const formData = new FormData(event.target);
+    name = formData.get("name");
+
+    webSocket.send(`${name} says hello world`);
+}
+
+function handleSend(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const chat = formData.get("chat");
+
+    webSocket.send(`${name}: ${chat}`);
+}
+
+function handleLeave() {
+    const lastChat = `${name} says goodbye world`
+
+    webSocket.send(lastChat);
+
+    const optimisticallyRenderedChat = document.createElement("p");
+    optimisticallyRenderedChat.textContent = lastChat;
+    chatBox.prepend(optimisticallyRenderedChat);
+
+    webSocket.close();
+}
--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const port = process.env.PORT || 9000;
 const httpServer = app.listen(port, () => console.log(`Listening on port ${port}`));
 const websocket = new WebSocketServer({ httpServer });
 
+app.use(express.static(__dirname + '/frontend'));
+
 app.get("/", (request, response) => {
-    response.send("This is running!");
+    response.render('index.html');
 })
 
 websocket.on("request", handleRequest);
@@ -22,17 +24,15 @@ function handleRequest(request) {
 
     connection.on("message", (message) => handleMessage(message, connections));
   
-    pokeEvery5Seconds(connection);
+    pokeEvery15Seconds(connection);
 }
 
 function handleMessage(message, connections) {
-    console.log(`The message: ${message.utf8Data}`);
-    
-    connections.forEach(connection => connection.send(`Yo we heard ${message.utf8Data}`));
+    connections.forEach(connection => connection.send(message.utf8Data));
 }
 
-function pokeEvery5Seconds(connection) {
+function pokeEvery15Seconds(connection) {
     connection.send(`You owe me ${ Math.random() }`);
 
-    setTimeout(() => pokeEvery5Seconds(connection), 5000);
+    setTimeout(() => pokeEvery15Seconds(connection), 15000);
 }
