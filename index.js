5301032d6d29ec6c17590e19461226cd22675add
LimitToInfinity <21stLimitless@gmail.com>
2020-04-07 21:14:45 -0600

    Cleaned up code order, and created connetions array to send all incoming messages to each connection.

--- index.js
+++ index.js
@@ -4,28 +4,31 @@ const WebSocketServer = require("websocket").server;
 const app = express();
 const port = process.env.PORT || 9000;
 const httpServer = app.listen(port, () => console.log(`Listening on port ${port}`));
+const websocket = new WebSocketServer({ httpServer });
 
 app.get("/", (request, response) => {
-    
     response.send("This is running!");
 })
-const websocket = new WebSocketServer({ httpServer });
+
 websocket.on("request", handleRequest);
 
+const connections = [];
 function handleRequest(request) {
     const connection = request.accept(null, request.origin);
+    connections.push(connection);
 
     connection.on("open", () => console.log("We OPEN!!!"));
     connection.on("close", () => console.log("We SHUT IT DOWWNNNNN!!!"));
-    connection.on("message", (message) => handleMessage(message, connection));
+
+    connection.on("message", (message) => handleMessage(message, connections));
   
     pokeEvery5Seconds(connection);
 }
 
-function handleMessage(message, connection) {
+function handleMessage(message, connections) {
     console.log(`The message: ${message.utf8Data}`);
     
-    connection.send(`Yo we heard ${message.utf8Data}`);
+    connections.forEach(connection => connection.send(`Yo we heard ${message.utf8Data}`));
 }
 
 function pokeEvery5Seconds(connection) {
